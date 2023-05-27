e6e32a3a4052e42cc69d859887461f58efe7eeb5
xilujain <lujainboo@gmail.com>
2023-05-27 21:28:09 +0300

    update: delete customer reservation

--- bin/Response/Owner/cancelReservation.dart
+++ bin/Response/Owner/cancelReservation.dart
@@ -1,24 +1,27 @@
 // ignore_for_file: file_names
 
 import 'dart:convert';
+import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
 import 'package:shelf/shelf.dart';
 import '../../ResponseMsg/CustomResponse.dart';
 import '../../Services/Supabase/SupabaseEnv.dart';
 
 cancelReservationOwnerResponse(Request req) async {
   try {
-    final body = json.decode(await req.readAsString()); //json - meeting room
+    final body =
+        json.decode(await req.readAsString()); //json - ex: meeting room
     final supabase = SupabaseEnv().supabase;
-    final usernameCustomer = supabase
+    final jwt = JWT.decode(req.headers["authorization"]!);
+    final reservationId = supabase
         .from('reservations')
-        .select('product-type')
-        .eq('customer-username', body['username']);
+        .select('id')
+        .eq("id_auth", jwt.payload["sub"]);
 
     await supabase
         .from('reservations')
         .delete()
-        .eq('customer-username', usernameCustomer)
-        .select('product-type');
+        .eq('id', reservationId)
+        .eq('product-type', body['type']);
 
     return CustomResponse().successResponse(msg: "deleted");
   } catch (error) {
